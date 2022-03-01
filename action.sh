db184d5479dea0f3e90a113b2bc5e01a39ed3e4b
Tyler Young <tyler.young@stirlinglabs.com>
2022-03-01 15:13:58 -0500

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,35 +1,33 @@
-
+#!/usr/bin/bash
 checkout-storage-branch(){
 	declare -n worktree_path=$1
 	
 	# get default remote url
-	remote_url=$(git remote get-url origin)
+	#remote_url=$(git remote get-url origin)
 
 	# clone repo in another directory
 	worktree_path=/tmp/$storage_branch-$(date +%s)
 	
 	# if the branch already exists, grab it
-	git fetch -f --update-shallow origin $storage_branch
-
-	if [ $? -ne 0 ]; then
+	if ! (git fetch -f --update-shallow origin "$storage_branch"); then
 		echo "Creating storage branch: $storage_branch"
 		# virtually create an orphan branch
 		empty_tree=$(git hash-object -w -t tree /dev/null)
 		empty_commit=$(git commit-tree "$empty_tree" -m "Created storage branch.")	
 
 		# create the worktree's directory
-		mkdir -p $worktree_path
+		mkdir -p "$worktree_path"
 		
 		# create a worktree for the orphan
-		git worktree add -b $storage_branch $worktree_path $empty_commit || exit $?	
+		git worktree add -b "$storage_branch" "$worktree_path" "$empty_commit" || exit $?	
 	else
 		echo "Using storage branch: $storage_branch"
 		# use the existing branch
-		git worktree add --checkout --track $worktree_path $storage_branch || exit $?
+		git worktree add --checkout --track "$worktree_path" "$storage_branch" || exit $?
 	fi
 	
 	# return the worktree path
-	echo $worktree_path
+	echo "$worktree_path"
 }
 
 append-storage(){
@@ -40,17 +38,17 @@ append-storage(){
 	abs_dst_path=$storage_local_path/$dst
 
 	# create it if it doesn't exist
-	if [[ "${dst:~0}" -eq "/" ]] ; then
-		mkdir -p $abs_dst_path
+	if [[ "${dst: -1}" = '/' ]] ; then
+		mkdir -p "$abs_dst_path"
 	else
-		mkdir -p $(dirname $abs_dst_path)
+		mkdir -p "$(dirname "$abs_dst_path")"
 	fi
 	
 	# copy the new content over
-	cp -r $src $abs_dst_path || exit $?
+	cp -r "$src" "$abs_dst_path" || exit $?
 
 	# get back to the worktree root
-	cd $storage_local_path || exit $?
+	cd "$storage_local_path" || exit $?
 
 	# add, commit and push the new content
 	git add . || exit $?
@@ -71,20 +69,20 @@ prune-storage(){
 	abs_dst_path=$storage_local_path/$dst
 
 	# wipe out the existing directory
-	rm -rf $abs_dst_path
+	rm -rf "$abs_dst_path"
 
 	# recreate it
-	if [[ "${dst:~0}" -eq "/" ]] ; then
-		mkdir -p $abs_dst_path
+	if [[ "${dst: -1}" = '/' ]] ; then
+		mkdir -p "$abs_dst_path"
 	else
-		mkdir -p $(dirname $abs_dst_path)
+		mkdir -p "$(dirname "$abs_dst_path")"
 	fi
 	
 	# copy the new content over
-	cp -r $src $abs_dst_path || exit $?
+	cp -r "$src" "$abs_dst_path" || exit $?
 
 	# get back to the worktree root
-	cd $storage_local_path || exit $?
+	cd "$storage_local_path" || exit $?
 
 	# add, commit and push the new content
 	git add -A . || exit $?
@@ -93,7 +91,7 @@ prune-storage(){
 	( git commit -m "$comment" && git push origin "HEAD:$storage_branch" ) || echo "No changes in storage branch." > /dev/stderr
 	
 	# go back to the working directory
-	cd -
+	cd - || exit $?
 }
 
 
@@ -103,7 +101,7 @@ main(){
 		apiUrl="https://api.github.com/users/$GITHUB_ACTOR"
 		userId=$( curl -H "Authorization: token $GITHUB_TOKEN" -H "$acceptHead" "$apiUrl" | jq '.id' )
 		git config --global user.name "$GITHUB_ACTOR"
-		if [[ -n "$userId" ]] || [[ "$userId" -eq "null" ]]; then
+		if [[ -n "$userId" ]] || [[ "$userId" = "null" ]]; then
 			git config --global user.email "$userId+$GITHUB_ACTOR@users.noreply.github.com"
 		else
 			git config --global user.email "branch.storage@github.action"
@@ -118,24 +116,24 @@ main(){
 	echo "Local storage path: $storage_local_path" > /dev/stderr
 	
 	if [[ "$prune" != 'true' ]]; then
-		append-storage $storage_local_path
+		append-storage "$storage_local_path"
 	else
-		prune-storage $storage_local_path
+		prune-storage "$storage_local_path"
 	fi
 
-	cd $storage_local_path || exit $?
+	cd "$storage_local_path" || exit $?
 
-	storage_branch=$(git branch --show-current)
+	storage_branch="$(git branch --show-current)"
 
 	cd - || exit $?
 
 	echo "Removing local storage branch $storage_branch and worktree: $storage_local_path"
 
-	git worktree remove -f $storage_local_path || exit $?
+	git worktree remove -f "$storage_local_path" || exit $?
 
-	git branch -D $storage_branch || exit $?
+	git branch -D "$storage_branch" || exit $?
 
-	rm -rf $storage_local_path || exit $?
+	rm -rf "$storage_local_path" || exit $?
 }
 
 main
